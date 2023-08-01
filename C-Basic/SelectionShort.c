feabb151670052c41506b23358ec8f3d9969da52
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2023-08-01 09:44:02 +0530

    1-8-23

--- C-Basic/SelectionShort.c
+++ C-Basic/SelectionShort.c
@@ -1,16 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
-# define n 15
+#include <stdio.h>
+#include <stdlib.h>
+#define n 15
 int a[n];
-int Display(){
-    for(int i=0;i<n;i++)
-        printf("%d  ",a[i]);
+int Display()
+{
+    for (int i = 0; i < n; i++)
+        printf("%d  ", a[i]);
 }
-int SelectionShort(){
+int SelectionShort()
+{
     int i, j, t;
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(a[i]>a[j]){
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
                 t = a[i];
                 a[i] = a[j];
                 a[j] = t;
@@ -18,8 +23,9 @@ int SelectionShort(){
         }
     }
 }
-int main(){
-    for(int i=0;i<n;i++)
+int main()
+{
+    for (int i = 0; i < n; i++)
         a[i] = rand() % 50 + 1;
 
     printf("\nRandom Array is : ");
